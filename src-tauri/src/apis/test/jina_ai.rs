19759caf275f60d49bdd16994e53a4fe83309a20
Noam Siegel <52804845+noamsiegel@users.noreply.github.com>
2024-09-17 15:22:31 -0700

    Remove unused code in jina_ai.rs

--- src-tauri/src/apis/test/jina_ai.rs
+++
@@ -1,9 +0,0 @@
-use tauri::
-
-#[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
-    let res = reqwest::get("https://r.jina.ai/https://example.com").await?;
-    println!("Status: {}", res.status());
-
-    Ok(())
-}
