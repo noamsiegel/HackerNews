b5f1628fb64bac9e4bbaf44649faa1abcd52af46
Noam Siegel <52804845+noamsiegel@users.noreply.github.com>
2024-09-18 19:41:22 -0700

    Made everything look better

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<!-- <script>import "../app.css";</script><slot></slot> -->
+<script lang="ts">
+  import "../app.pcss";
+  import Settings from "./settings/+page.svelte";
+  import * as Sheet from "$lib/components/ui/sheet";
+  import { settingsOpen } from "$lib/stores/settingsStore";
+</script>
+
+<slot />
+
+<Sheet.Root bind:open={$settingsOpen}>
+  <Sheet.Content>
+    <Settings />
+  </Sheet.Content>
+</Sheet.Root>
